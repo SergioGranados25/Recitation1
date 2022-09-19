bc3ddaffa424947724df36a28a6858e82b47f0d1
ssgranados21students.desu.edu <ssgranados21@students.desu.edu>
2022-09-18 22:59:03 -0400

    Final work

--- cypher.py
+++ cypher.py
@@ -11,9 +11,27 @@ ordinal_value = {ch: i for i, ch in enumerate(alphabet)}
 
 def encrypt(message, password):
     encrypted_message = ''
-    pass  # TODO replace this line with your code.
+    for index, ch in enumerate(message):
+        pass_ch = password[index % len(password)]
+        key = ordinal_value[pass_ch]
+        ord_ch = ordinal_value[ch]
+        shifted_ord_ch = (ord_ch + key) % len(alphabet)
+        encrypted_ch = alphabet[shifted_ord_ch]
+        encrypted_message += encrypted_ch
+    return encrypted_message
+
 
 
 def decrypt(message, password):
     decrypted_message = ''
-    pass  # TODO replace this line with your code.
+    for index, ch in enumerate(message):
+        pass_ch = password[index % len(password)]
+        key = ordinal_value[pass_ch]
+        ord_ch = ordinal_value[ch]
+        shifted_ord_ch = (ord_ch - key) % len(alphabet)
+        decrypted_ch = alphabet[shifted_ord_ch]
+        decrypted_message += decrypted_ch
+    return decrypted_message
+
+print(encrypt("This is a test, hello world!", "Mypass"))
+print(decrypt(encrypt("This is a test, hello world!", "Mypass"),"Mypass"))
